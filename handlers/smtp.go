c3960b25ce2a144c8920ba5e6130b63cbf8eb772
diggi404 <dbackson1@gmail.com>
2023-07-16 17:45:31 -0400

    hid smtp credentials

--- handlers/smtp.go
+++ handlers/smtp.go
@@ -1,11 +1,19 @@
 package handlers
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gopkg.in/gomail.v2"
 )
 
 var Smtp *gomail.Dialer
 
 func ConnectSmtp() {
-	Smtp = gomail.NewDialer("smtp.gmail.com", 587, "karianfavreau9@gmail.com", "bcmvowqxuqomtmza")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file!")
+	}
+	Smtp = gomail.NewDialer(os.Getenv("SMTP_HOST"), 587, os.Getenv("SMTP_USER"), os.Getenv("SMTP_PASSWORD"))
 }
